9411de8837a97865d71840a4f4c555f0118f742b
darshan-kt <darshankt186@gmail.com>
2025-09-22 15:11:30 +0200

    added openpose and human_parse test results

--- docs/backend.sh
+++ docs/backend.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Set GVirtuS home directory
-export GVIRTUS_HOME=/home/darshan/GVirtuS
+export GVIRTUS_HOME=/home/username/GVirtuS
 
 # Run gvirtus-backend with updated LD_LIBRARY_PATH
 LD_LIBRARY_PATH="${GVIRTUS_HOME}/lib:${LD_LIBRARY_PATH}" \
